a3479006204327529175fc1426c7beaad529a6ba
DreamOneX <1538874738@qq.com>
2021-12-07 19:23:03 +0800

    添加布尔型，字符型，浮点型，双浮点型，无类型，宽字符型，短，长，有符号，无符号，定义类型，枚举

--- chscpp.hpp
+++ chscpp.hpp
@@ -2,7 +2,25 @@
 #define 命名空间 namespace
 #define 使用 using
 #define 标准 std
+
+//数据类型
 #define 整型 int
+#define 布尔型 bool
+#define 字符型 char
+#define 浮点型 float
+#define 双浮点型 double
+#define 无类型 void
+#define 宽字符型 twchar_t
+
+#define 短 short
+#define 长 long
+#define 有符号 signed
+#define 无符号 unsigned
+
+#define 定义类型 typedef
+#define 枚举 enum
+//到这里
+
 #define 主啊 main
 #define 参数个数 argc
 #define 参数们 argv
