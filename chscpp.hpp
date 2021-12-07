22b3b4a01fb4a674e10af570dfc687230f17027b
DreamOneX <1538874738@qq.com>
2021-12-07 13:24:46 +0800

    添加了 命名空间，使用，标准，整型，主啊，参数个数，参数们，输出，换行，返回

---
+++ chscpp.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define 命名空间 namespace
+#define 使用 using
+#define 标准 std
+#define 整型 int
+#define 主啊 main
+#define 参数个数 argc
+#define 参数们 argv
+#define 输出 cout
+#define 换行 endl
+#define 返回 return
